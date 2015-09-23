607f361304719f6e546c9f59e6373ca8330deb2c
Simon <Sim.Leyder@gmail.com>
2015-09-23 14:02:47 +0200

    hide/show navigation bar when scroll

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,16 +1,45 @@
-/* Name-Project-Here
+/* Portfolio 2015
  *
- * Script utilities
+ * Script uses for adding interactivity for my portfolui
  *
- * Started @ JJ/MM/AAAA
+ * Started @ 23/09/2015
  */
 
 ( function( $ ) {
     "user strict";
 
-    $( function() {
-        // Call my function here
-        
+    // Hidden / Show menu when scroll
+    var previousScroll = 0;
+
+    $( window ).scroll( function()
+    {
+        var currentScroll = $( this ).scrollTop();
+
+        if ( currentScroll > 0 && currentScroll < $( document ).height() - $( window ).height() )
+        {
+
+            if( currentScroll > previousScroll )
+            {
+                window.setTimeout( hideNav, 250 );
+            }
+            else
+            {
+                window.setTimeout( showNav, 250 );
+            }
+
+            previousScroll = currentScroll;
+      }
+
     } );
+
+    function hideNav()
+    {
+      $( ".header" ).removeClass( "header--visible" ).addClass( "header--hidden" );
+    }
+
+    function showNav()
+    {
+      $( ".header" ).removeClass( "header--hidden" ).addClass( "header--visible" );
+    }
 
 } ) (jQuery);
