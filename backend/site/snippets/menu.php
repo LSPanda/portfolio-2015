c73249fd1b11edb4f33187699d99fe50f9de3ebe
Simon <Sim.Leyder@gmail.com>
2015-10-27 15:14:54 +0100

    home page : kirby

--- backend/site/snippets/menu.php
+++ backend/site/snippets/menu.php
@@ -2,6 +2,6 @@
   <h2 class="hidden">Navigation à travers mon portfolio</h2>
     <span class="navigation__element"><a href="<?php echo( url() ) ?>" class="element__link animateLink">Accueil</a></span>
     <?php foreach( $pages->visible() as $p ): ?>
-        <span class="navigation__element"><a href="<?php echo $p->url() ?>" class="element__link animateLink"><?php echo $p->title()->html() ?></a></span>
+        <span class="navigation__element"><a href="<?php echo( $p->url() ) ?>" class="element__link animateLink"><?php echo( $p->title()->html() ) ?></a></span>
     <?php endforeach; ?>
 </nav>
--- backend/site/templates/home.php
+++ backend/site/templates/home.php
@@ -1,11 +1,12 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
-
-    <p>
-        prout
-    </p>
-
-  </main>
-
+    <div class="cover">
+        <img src="assets/images/logo.svg" alt="Logo de Simon&amp;You. Code, design and Print" class="cover__logo">
+        <img src="assets/images/animeMouse.svg" alt="Animation représentant une souris d’ordinateur" class="cover__mouse">
+        <img src="assets/images/animeArrow.svg" alt="Animation représentant une souris d’ordinateur" class="cover__arrow bounce">
+    </div>
+    <div class="wrapper__text">
+        <h2 class="hidden">Bienvenu chez moi&nbsp;!</h2>
+        <?php echo $page->text() ?>
+    </div>
 <?php snippet('footer') ?>
